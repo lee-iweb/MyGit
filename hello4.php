fbfc51856e8af499833c434f8952e4c32797fad0
admin <admin@192.168.3.82>
2012-02-20 14:52:47 +0800

    hello4

---
+++ hello4.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Created on 2012-2-20
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ echo 'hello4';
+?>
